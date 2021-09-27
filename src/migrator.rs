6128cb7630b5122075c431faa65bdee8f06a565c
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2021-09-27 12:08:53 -0400

    remove unnecessary files

--- src/migrator.rs
+++ src/migrator.rs
@@ -1,12 +1,9 @@
 use super::Connection;
+use super::Transaction;
 mod migrationdirection;
 use migrationdirection::MigrationDirection;
-mod domigrations;
-use domigrations::DoMigrations;
 mod migration;
 use migration::Migration;
-mod migratoraccess;
-use migratoraccess::MigratorAccess;
 /// A higher-level object to manage migration statuses
 pub struct Migrator<'m> {
     /// The db path
@@ -18,18 +15,13 @@ pub struct Migrator<'m> {
     /// A count of skipped migrations
     skip_count: usize,
 }
-impl<'m> MigratorAccess for Migrator<'m> {
+impl<'m> Migrator<'m> {
     fn inc_skip_count(&mut self) {
         self.skip_count = self.skip_count + 1;
-    }
-    fn access_skip_count(&mut self) -> &mut usize {
-        return &mut self.skip_count;
     }
     fn access_connection(&mut self) -> &mut Connection {
         return &mut self.connection;
     }
-}
-impl<'m> Migrator<'m> {
     /// Retrieves the number of migrations skipped due to unpassed checks (not innately an issue)
     pub fn get_skip_count(&self) -> usize {
         return self.skip_count.clone();
@@ -145,6 +137,80 @@ impl<'m> Migrator<'m> {
             };
         }
         return res;
+    }
+    /// Runs the passed Migration's check script
+    fn query_chk(c: &Connection, m: &Migration) -> Result<i64, String> {
+        let mut chk_stmt = match c.prepare(&m.check) {
+            Ok(stmt) => stmt,
+            Err(e) => return Err(format!("Failed to prepare statement: {}", e)),
+        };
+        match chk_stmt.query_row([], |row| row.get(0)) {
+            Ok(i) => return Ok(i),
+            Err(e) => return Err(format!("Failed to query rows from statement: {}", e)),
+        };
+    }
+    /// Runs the applicable script of a Migration based on the direction
+    fn run_migration(t: &mut Transaction, m: &Migration, d: &MigrationDirection) -> Result<(), String> {
+        let s = match d {
+            &MigrationDirection::Up => {
+                m.up.clone()
+            },
+            &MigrationDirection::Down => {
+                m.down.clone()
+            },
+        };
+        match t.execute(&s, []) {
+            Ok(_) => return Ok(()),
+            Err(e) => return Err(format!("Failed to execute script: {}", e)),
+        };
+    }
+    /// Migrates the SQLite database in the given direction
+    fn migrate(&mut self, direction: MigrationDirection, mig_path: String) -> Result<usize, String> {
+        let mut migrations = match Migration::get_all(mig_path) {
+            Ok(migrations) => migrations,
+            Err(e) => return Err(e),
+        };
+        let passing_int: i64;
+        if direction.eq(&MigrationDirection::Down) {
+            migrations.sort_unstable_by(|x, y| y.number.cmp(&x.number));
+            passing_int = 1;
+        } else {
+            migrations.sort_unstable_by(|x, y| x.number.cmp(&y.number));
+            passing_int = 0;
+        }
+        for migration in migrations {
+            let passing_check = match Self::query_chk(&self.access_connection(), &migration) {
+                Ok(i) => i,
+                Err(e) => return Err(e),
+            };
+            if !passing_int.eq(&passing_check) {
+                self.inc_skip_count();
+                println!("Skipping {}", migration.file_name);
+                continue;
+            }
+            println!("Running '{}'", migration.file_name);
+            let mut tx = match self.access_connection().transaction() {
+                Ok(tx) => tx,
+                Err(e) => return Err(format!("Failed to create transaction from connection: {}", e)),
+            };
+            match Self::run_migration(&mut tx, &migration, &direction) {
+                Ok(_) => {},
+                Err(e) => return Err(e),
+            }
+            match tx.commit() {
+                Ok(_) => continue,
+                Err(e) => return Err(format!("Failed to commit transaction: {}", e)),
+            }
+        }
+        return Ok(self.get_skip_count());
+    }
+    /// Migrates the SQLite database upward
+    fn up<'b>(&mut self, mig_path: &'b str) -> Result<usize, String> {
+        return self.migrate(MigrationDirection::Up, mig_path.to_string());
+    }
+    /// Migrates the SQLite database downward
+    fn down<'b>(&mut self, mig_path: &'b str) -> Result<usize, String> {
+        return self.migrate(MigrationDirection::Down, mig_path.to_string());
     }
     #[cfg(test)]
     fn do_both<'a>(migrations_path: &'a str, db_path: &'a str, db_name: &'a str) -> Result<usize, String> {

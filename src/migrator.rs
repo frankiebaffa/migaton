956ef336443c22033aeb1b52291b50d0191ef473
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2021-10-19 09:07:06 -0400

    debug print

--- src/migrator.rs
+++ src/migrator.rs
@@ -86,6 +86,7 @@ impl<'m> Migrator<'m> {
     }
     /// Runs the passed Migration's check script
     fn query_chk(c: &Connection, m: &Migration) -> Result<i64, String> {
+        println!("{}", &m.check);
         let mut chk_stmt = match c.prepare(&m.check) {
             Ok(stmt) => stmt,
             Err(e) => return Err(format!("Failed to prepare statement: {}", e)),
